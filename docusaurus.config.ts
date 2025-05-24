56c86726e68abe4b05c3300ed7e7ee77b634e03c
ssan9woo <ssan9woo@gmail.com>
2025-05-24 10:58:32 +0900

    config 변경

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,6 +15,8 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
+  deploymentBranch: 'gh-pages',
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ssan9woo',
